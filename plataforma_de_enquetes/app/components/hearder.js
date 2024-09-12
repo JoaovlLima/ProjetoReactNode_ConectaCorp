9e10109d05129ea426e3f1933ae008d5719edd4b
JoaovlLima <jl949615@gmail.com>
2024-09-12 14:05:41 -0300

    12.09

--- plataforma_de_enquetes/app/components/hearder.js
+++ plataforma_de_enquetes/app/components/hearder.js
@@ -8,7 +8,7 @@ export default function Header() {
         <img src="/logo.png" alt="Logo" />
       </div>
       <div className={styles.fields}>
-        <a href='' title='Criar Enquete'
+        
         <input type="text" placeholder="Campo 2" />
         <input type="text" placeholder="Campo 3" />
       </div>
